af62519752c88269f1d1008581d55b81c1b91217
aaron miguel cardenas <2242828@slu.edu.ph>
2025-04-24 16:15:29 +0800

    AppointmentDAO done

--- src/util/Appointment.java
+++ src/util/Appointment.java
@@ -6,8 +6,46 @@ public class Appointment {
     private int appointmentID;
     private LocalDateTime appointmentDate;
     private String status;
-    private int duration;// TODO: Data type to be finalized.
+    private int duration; // in minutes
     private LocalDateTime createdDate;
 
-    //Constructors & other methods to be added til attributes are finalized.
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(int appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
